ca72f7cba0c0c5ffcef06d2f332c2adf92e3b80c
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-23 13:55:23 +0100

    icon component

--- src/app/components/InfoOptions.js
+++ src/app/components/InfoOptions.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ReactSVG from 'react-svg';
+import Like from './svg/Like';
 
 export default (props) => {
     return (
         <div className="results__option">
             <a href={props.info.links.html} className="link link--grey results__option--like">
-                {props.info.likes}
+                    <Like
+                        name="like"
+                        className="results__option--icon"
+                    />
+                    {props.info.likes}
             </a>
             <a className="link link--grey results__option--download" href={props.info.links.download} download>
                 <span>
---
+++ src/app/components/svg/Like.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { paths } from './../../constants';
+
+export default function icon(props) {
+    const name = props.name;
+    const iconClass = props.className;
+
+    return (
+        <svg
+            className={iconClass}
+        >
+            <use xlinkHref={`${paths.GLOBAL}/gfx/svg/sprites/icons.svg#${name}`} />
+        </svg>
+    );
+}
