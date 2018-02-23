70df27f2eca9a15002d9fc423c3a0dbf1ebb5fc2
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-02-23 09:22:38 +0100

    modal window

--- src/app/components/ModalWindow.js
+++ src/app/components/ModalWindow.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import InfoOptions from './InfoOptions';
 
 export default (props) => {
     return (
-        <div className="modal">
-            <figure className="modal__figure">
-                <picture>
-                    <img className="modal__image" src={props.props.urls.regular} alt="" />
-                </picture>
-            </figure>
+        <div className="js-modal modal">
+            <div className="modal__content">
+                <div className="modal__item">
+                    <div className="modal__profile">
+                        <div className="modal__info--profile">
+                            <picture>
+                                <a href={props.image.user.links.html}>
+                                    <img className="modal__info--photo" src={props.image.user.profile_image.small} alt="Unsplash" />
+                                </a>
+                            </picture>
+                            <p className="text-left">
+                                <a href={props.image.user.links.html} className="modal__info--link link link--grey">
+                                    @{props.image.user.username}
+                                </a>
+                            </p>
+                        </div>
+                        <InfoOptions info={props.image} />
+                    </div>
+                    <figure className="modal__figure">
+                        <picture>
+                            <img className="modal__image" src={props.image.urls.regular} alt="Unsplash" />
+                        </picture>
+                        <figcaption className="modal__location">
+                            {props.image.user.location}</figcaption>
+                    </figure>
+                </div>
+            </div>
         </div>
     );
 };
