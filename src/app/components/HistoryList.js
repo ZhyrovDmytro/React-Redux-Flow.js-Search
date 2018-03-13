75a6017d8e8389952d81b3760a9ef643ea3dd882
DmytroZhyrov <dmitriy_zhirov@mail.ru>
2018-03-13 11:24:38 +0100

    search by hoistory item

--- src/app/components/HistoryList.js
+++ src/app/components/HistoryList.js
@@ -6,11 +6,16 @@ export default (props) => {
             <li
                 key={`_${historyItem.id}${Math.random().toString(36).slice(-5)}`}
                 className="history__item"
+                onMouseDown={() => props.searchByHistory(historyItem)}
+                role="menuitem"
             >
-                <span>{ historyItem }</span>
+                <span>
+                    { historyItem }
+                </span>
             </li>
         );
     });
+
     return (
         <div className="history">
             <ul className="history__list">
